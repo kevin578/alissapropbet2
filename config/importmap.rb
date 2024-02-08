d8ac24b4fde8aadaef9ece53abe951a772647651
Kevin Briggs <kevinbriggs1@gmail.com>
2024-02-07 21:26:58 -0500

    update cdn

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,7 +2,7 @@
 
 ##{Rails.root}/node_modules/tw-elements/dist/js/index.min.js
 pin "application", preload: true
-pin "tw-elements", to: "https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"
+pin "tw-elements", to: "https://cdn.jsdelivr.net/npm/tw-elements@1.0.0-alpha13/dist/js/index.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/helpers", under: "helpers"
